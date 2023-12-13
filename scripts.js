9d7f24600583739eb43fde9855f1d9bd673c5a73
Belinda Santillan <belindasarahisantillan@gmail.com>
2023-12-13 13:14:36 -0600

    Format Document

--- scripts.js
+++ scripts.js
@@ -1,51 +1,57 @@
-
 let contrastToggle = false;
 let isModalOpen = false;
-const scaleFactor = 1/20;
+const scaleFactor = 1 / 20;
 
 function moveBackground(event) {
-    const shapes = document.querySelectorAll(".shape");
-    const x = event.clientX * scaleFactor;
-    const y = event.clientY * scaleFactor;
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
 
-    for (let i = 0; i < shapes.length; ++i) {
-        const isOdd = i % 2 !== 0;
-        const boolInt = isOdd ? -1 : 1;
-        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
-    }
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
 }
 
 function toggleContrast() {
-    contrastToggle = !contrastToggle;
-    if (contrastToggle) {
-        document.body.classList += " dark-theme";
-    }
-    else {
-        document.body.classList.remove("dark-theme");
-    }
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
 }
 
 function contact(event) {
-    event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading');
-    const success = document.querySelector('.modal__overlay--success');
-    loading.classList += " modal__overlay--visible";
-    emailjs.sendForm('service_un2gujc','template_06945xr', event.target, 'AWfJpaeLyK7K6YIZC').then(() => {
-            loading.classList.remove("modal__overlay--visible");
-            success.classList += " modal__overlay--visible";}).catch(() => {
-            loading.classList.remove("modal__overlay--visible");
-            alert(
-                "The email server is temporarily unavailable. Please to contact directly at belindasarahisantillan@gmail.com"
-            );
-        })
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_un2gujc",
+      "template_06945xr",
+      event.target,
+      "AWfJpaeLyK7K6YIZC"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email server is temporarily unavailable. Please to contact directly at belindasarahisantillan@gmail.com"
+      );
+    });
 }
 
-
 function toggleModal() {
-    if (isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open");
-    }
-    isModalOpen = true;
-    document.body.classList += " modal--open";
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
